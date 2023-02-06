67c1028cf5113d4ef6a19231e11175432ff04bbc
davisnoah <davisnoah8079@gmail.com>
2023-02-06 15:05:46 -0500

    Set up csv database parsing code
    - Install node-csv module
    - Create demo csv database
    - Write beginnings of code to parse data

--- server/server.js
+++ server/server.js
@@ -1,2 +1,22 @@
-const { readFile, writeFile } = require("fs");
+const fs = require('fs');
+const { parse } = require('csv-parse');
+const { stringify } = require('csv-stringify');
+const dataAddress = __dirname+"/database/tasks.csv";
 
+const getParsedData = (location) => {
+  let parsedData = [];
+  fs.createReadStream(dataAddress)
+  .pipe(parse({ delimiter: ",", from_line: 2 }))
+  .on("data", function (row) {
+    console.log(row);
+  })
+  .on("end", function () {
+    console.log("finished");
+  })
+  .on("error", function (error) {
+    console.log(error.message);
+  });
+  return parsedData;
+}
+
+const parsedData = getParsedData(dataAddress);
