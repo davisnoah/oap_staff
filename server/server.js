67fa883509e570815ac53d4215f98953381474a9
davisnoah <davisnoah8079@gmail.com>
2023-02-08 23:01:53 -0500

    Set up csv file writing code

--- server/server.js
+++ server/server.js
@@ -1,22 +1,22 @@
-const fs = require('fs');
-const { parse } = require('csv-parse');
+const fs = require('fs').promises;
+const { parse } = require('csv-parse/sync');
 const { stringify } = require('csv-stringify');
-const dataAddress = __dirname+"/database/tasks.csv";
 
-const getParsedData = (location) => {
-  let parsedData = [];
-  fs.createReadStream(dataAddress)
-  .pipe(parse({ delimiter: ",", from_line: 2 }))
-  .on("data", function (row) {
-    console.log(row);
-  })
-  .on("end", function () {
-    console.log("finished");
-  })
-  .on("error", function (error) {
-    console.log(error.message);
-  });
-  return parsedData;
+const tasksAddress = __dirname+"/database/tasks.csv";
+
+const parseCSV = async (location) => {
+  const fileContent = await fs.readFile(location);
+  const records = parse(fileContent, {columns: true});
+  return records;
 }
 
-const parsedData = getParsedData(dataAddress);
+const updateCSV = (parsedData, csvAddress) => {
+  stringify(parsedData, {header: true}, function (err, output) {
+    fs.writeFile(csvAddress, output);
+  });
+}
+
+(async function main() {
+  let parsedTasks = await parseCSV(tasksAddress);
+  console.log(parsedTasks);
+})();
