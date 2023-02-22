85bab8b61751a478b72cbecb2603e3c7950e506c
davisnoah <davisnoah8079@gmail.com>
2023-02-22 16:23:16 -0500

    Put public folder and all server files and folders in main directory

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const { clientList, taskList } = require('./list').lists;
 const express = require('express');
 const app = express();
-
 const PORT = 8080;
-
-console.log(clientList);
-console.log(taskList);
 
 app.listen(PORT, '127.0.0.1', function() {
   console.log('Server listening on port ' + PORT);
